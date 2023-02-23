1fc5b8b07a58a9f4475fcfed6da4718b4eb10aa2
Jiyeon <gjy30533@naver.com>
2023-02-23 15:29:16 +0900

    test

--- BookZzuk/src/main/java/com/yedam/notice/service/NoticeService.java
+++ BookZzuk/src/main/java/com/yedam/notice/service/NoticeService.java
@@ -2,4 +2,11 @@ package com.yedam.notice.service;
 
 import java.util.List;
 
-public interface NoticeService {}
+import com.yedam.notice.vo.NoticeVO;
+
+public interface NoticeService {
+	public List<NoticeVO> noticeList();
+	public NoticeVO getNotice(String noticeNum);
+	
+
+}
