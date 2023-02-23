279c08d3df69211cba4a6101d76f52e6025080eb
asdzxc1062 <asdzxc1062@naver.com>
2023-02-23 17:52:16 +0900

    first

--- BookZzuk/src/main/java/com/yedam/member/service/MemberServiceImpl.java
+++ BookZzuk/src/main/java/com/yedam/member/service/MemberServiceImpl.java
@@ -1,11 +1,45 @@
 package com.yedam.member.service;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 
 import com.yedam.book.mapper.BookMapper;
 import com.yedam.common.DataSource;
+import com.yedam.member.vo.MemberVO;
 
 public class MemberServiceImpl implements MemberService {
 	SqlSession session = DataSource.getInstance().openSession(true);
 	BookMapper mapper = session.getMapper(BookMapper.class);
+	@Override
+	public MemberVO login(MemberVO member) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public int addMember(MemberVO member) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public List<MemberVO> memberList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public MemberVO getMember(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public int modifyMember(MemberVO member) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public int removeMember(String mid) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 }
+
--- BookZzuk/src/main/webapp/WEB-INF/views/member/login.jsp
+++ BookZzuk/src/main/webapp/WEB-INF/views/member/login.jsp
@@ -1,22 +1,23 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<h3>현재 페이지는 loginForm.do의 결과 login.jsp 입니다.</h3>
 
 
 <form action="login.do" method="post">
   <table class="table">
     <tr>
-      <th>ID</th><td><input type="text" name="mid"></td>
+      <th>ID</th><td><input type="text" name="bid"></td>
     </tr>
     <tr>
-      <th>Pass</th><td><input type="password" name="mpw"></td>
+      <th>Pass</th><td><input type="password" name="bpw"></td>
     </tr>
     <tr>
       <td colspan="2" align="center">
         <input type="submit" value="로그인">
         <input type="reset" value="취소">
+        <a href="#">아이디찾기</a>
+        <a href="#">비밀번호찾기</a>
       </td>
     </tr>
   </table>
-</form>
+</form>  
