62172b4cf60bc27b869101daf30b401874e095d0
taeseonghwang <shwlsgud86@naver.com>
2023-02-24 17:19:39 +0900

    asdf

--- BookZzuk/src/main/java/com/yedam/book/mapper/BookMapper.java
+++ BookZzuk/src/main/java/com/yedam/book/mapper/BookMapper.java
@@ -3,11 +3,14 @@ package com.yedam.book.mapper;
 import java.util.List;
 
 import com.yedam.book.vo.BookVO;
+import com.yedam.order.vo.OrderVO;
 
 public interface BookMapper {
 	
-	public List<BookVO>getBookList(String keyword,String SortmMod)
+	public List<BookVO>getBookList(String keyword,String SortmMod);
 
 	public List<BookVO> getLikeList(String uid);
+
+	public List<OrderVO> getAllOrder();
 	
 }
