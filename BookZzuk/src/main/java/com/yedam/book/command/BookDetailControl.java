5bb40eb4d09b9fd903f6acdda372ba95b55ef03d
5Solkun <kyj147852@gmail.com>
2023-02-24 17:29:37 +0900

    2.24퇴근이다~~~~~ 주말이지롱~~~~

--- BookZzuk/src/main/java/com/yedam/book/command/BookDetailControl.java
+++ BookZzuk/src/main/java/com/yedam/book/command/BookDetailControl.java
@@ -6,13 +6,22 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.yedam.book.service.BookService;
+import com.yedam.book.service.BookServiceImpl;
 import com.yedam.common.Command;
 
 public class BookDetailControl implements Command {
 
 	@Override
 	public String exec(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		return null;
+		String sbid=req.getParameter("bid");
+		int bid = Integer.parseInt(sbid) ;
+
+		BookService service = new BookServiceImpl();
+
+		req.setAttribute("book", service.getBook(bid));
+		req.setAttribute("related", service.getRelatedBook(bid));
+		return "book/bookDetail.tiles";
 	}
 
 }
--- BookZzuk/src/main/java/com/yedam/notice/service/NoticeService.java
+++ BookZzuk/src/main/java/com/yedam/notice/service/NoticeService.java
@@ -7,6 +7,7 @@ import com.yedam.notice.vo.NoticeVO;
 public interface NoticeService {
 	public List<NoticeVO> noticeList();
 	public NoticeVO getNotice(String noticeNum);
+	public Object getBook(int bid);
 	
 
 }
